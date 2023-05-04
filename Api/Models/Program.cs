915a43f8be9217cee14e6c7aacf88c3681de2706
Matt Chapman <matt@mattchapman.io>
2023-05-04 22:09:24 +1200

    Update swagger settings

--- Api/Models/Program.cs
+++ Api/Models/Program.cs
@@ -16,11 +16,8 @@ builder.Services.AddSingleton<IPuppyService, PuppyService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
